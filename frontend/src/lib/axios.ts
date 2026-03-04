2185d48f08e39c191940162faac9be5182bfc8b8
Huy Nguyen Minh <nguyenminhhuy@MacBook-Pro-cua-Nguyen.local>
2026-03-04 15:46:04 +0700

    Fix Vite PROD environment variable check

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.mode !== "production" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.PROD ? "/api" : "http://localhost:5001/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
